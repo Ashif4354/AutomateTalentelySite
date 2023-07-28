df785207b83bb11e3d276bd27022c1ca6ebfd368
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-07-29 01:35:35 +0530

    second commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import talently1 from "./talently1.png";
-import talently2 from "./talently2.png";
 import "./Header.css";
 
 const Header = () => {
     return(
-        <div className="header">    
-            {/* <div className="logos">
-                <img className="logoImg" src={talently1} alt="logo"/>
-                <img className="logoImg" src={talently2} alt="logo"/>
-            </div> */}
+        <div className="header">  
             <div className="heading">
                 <p>Automate Talentely - A Talentely Completion Service</p>
             </div>
