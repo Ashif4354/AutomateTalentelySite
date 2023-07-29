a08d0d17b5a0f5b16766056a899633f4c84d01f3
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-07-29 10:07:39 +0530

    added new line

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SecondHeader from './Components/SecondHeader/SecondHeader';
 function App() {
 
   const sendNotification = async () => {
- 
+
     const url = "https://discord.com/api/webhooks/1134570835802325115/D_ZiLMitVd0BSUI9HzxEUq-CCF9ypev-MqnroBTrjc9vVyZt8XLQeMJeDDTJ9mCPFWUh";
 
     const data = {
@@ -63,7 +63,10 @@ function App() {
         <ul>
           <li><p>
             It is a state-of-the-art online test attending application that revolutionizes the way students take up the online tests
-            Designed with students, professionals, and lifelong learners in mind, our platform is your one-stop destination for effortless and efficient test performance.</p>
+          </p>
+          </li>
+          <li>
+            <p>Designed with students, professionals, and lifelong learners in mind, our platform is your one-stop destination for effortless and efficient test performance.</p>
           </li>
           <li><p>It is an automated test attending service.</p>
           </li>
@@ -84,38 +87,38 @@ function App() {
         <p><strong>Disadvantage</strong></p>
         <ul>
 
-        <li><p>Does not attend programming tests.</p></li>
-        <li><p>Answering accuracy and time attendance may vary by 5%</p></li>
-        <li><p>Bugs may appear</p></li>
+          <li><p>Does not attend programming tests.</p></li>
+          <li><p>Answering accuracy and time attendance may vary by 5%</p></li>
+          <li><p>Bugs may appear</p></li>
         </ul>
         <p><strong>How the application works:</strong></p>
         <ul>
-          
-        <li><p>1. Gets credential from user and saves it</p></li>
-        <li><p>2. Opens browser</p></li>
-        <li><p>3. Log in with your account</p></li>
-        <li><p>4. Navigates to each test automatically</p></li>
-        <li><p>5. Starts and completes the test</p></li>
-        <li><p>6. Then navigate to next test and does the same</p></li>
-        <li><p>and goes on.....</p></li>
+
+          <li><p>1. Gets credential from user and saves it</p></li>
+          <li><p>2. Opens browser</p></li>
+          <li><p>3. Log in with your account</p></li>
+          <li><p>4. Navigates to each test automatically</p></li>
+          <li><p>5. Starts and completes the test</p></li>
+          <li><p>6. Then navigate to next test and does the same</p></li>
+          <li><p>and goes on.....</p></li>
         </ul>
         <p><strong>Why Choose our Service</strong></p>
         <ul>
 
-        <li><p>We understand that your time is valuable, and success hinges on proper organization and preparation.
-          More over this is a free application.</p></li>
+          <li><p>We understand that your time is valuable, and success hinges on proper organization and preparation.
+            More over this is a free application.</p></li>
           <li><p>Answering accuracy and time attendance may vary by 5%</p></li>
         </ul>
 
         <p><strong>Join our community today!</strong></p>
-      <ul>
-        <li><p>Experience the power and take charge of your academic and professional destiny. Don't let tests hold you back—conquer them with ease and finesse. Join the ranks of high achievers and thrive in the world of exams with us by your side.</p></li>
-        <li><p>Embark on this exciting journey with us and let's redefine success, together!</p></li>
-      </ul>
+        <ul>
+          <li><p>Experience the power and take charge of your academic and professional destiny. Don't let tests hold you back—conquer them with ease and finesse. Join the ranks of high achievers and thrive in the world of exams with us by your side.</p></li>
+          <li><p>Embark on this exciting journey with us and let's redefine success, together!</p></li>
+        </ul>
       </div>
       <div className='btn'>
         <button className='btnDownload' onClick={() => handleClick()}>DOWNLOAD</button>
-        <p id = 'version'>version: 7.5</p>
+        <p id='version'>version: 7.5</p>
       </div>
     </div>
   );
