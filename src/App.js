9bcf95c8be0d2f486fad90126e828018eb85dc25
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-08-01 18:46:09 +0530

    Version 8.4

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
       </div>
       <div className='btn'>
         <button className='btnDownload' onClick={() => handleClick()}>Download</button>
-        <p id='version'>version: 8.2</p>
+        <p id='version'>version: 8.4</p>
       </div>
     </div>
   );
