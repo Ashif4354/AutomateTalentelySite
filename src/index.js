527da746ad994162c56c82a2be7bfdae6ad6dda7
Arul Rosario <itsarrowhere380@gmail.com>
2023-08-02 00:00:31 +0530

    Version 8.4

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
