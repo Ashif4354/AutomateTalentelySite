f44aa0ff2ab4ae1c3d0a7ca1098b4d9f6eb83ea2
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-10-11 09:01:59 +0000

    started feedback

---
+++ src/Components/FeedBack/FeedBack.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import SecondHeader from "../ProblemStatement/Header/Header"
+
+const FeedBack = () => {
+    return (
+        <div>
+            <Header heading="Feedback"/>
+            <SecondHeader header="Feedback"/>
+        </div>
+    );
+}
+
+export default FeedBack;
